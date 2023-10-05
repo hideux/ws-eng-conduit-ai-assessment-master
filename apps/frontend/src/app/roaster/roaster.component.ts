fb0c0da349a51983357530606ec6163b66e87416
hideux <hideux@live.com>
2023-10-05 18:34:04 +0300

    roaster component created

---
+++ apps/frontend/src/app/roaster/roaster.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'cdt-roaster',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './roaster.component.html',
+  styleUrls: ['./roaster.component.scss'],
+})
+export class RoasterComponent {}
